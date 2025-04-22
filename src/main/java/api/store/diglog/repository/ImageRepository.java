b9320657f5b882bb392dd4efa42053ac3e97446e
typhoon0678 <typhoon0678@gmail.com>
2025-04-22 11:34:44 +0900

    refactor: ImageRepository의 deleteAll 메서드 @Modifying 어노테이션 제거

--- src/main/java/api/store/diglog/repository/ImageRepository.java
+++ src/main/java/api/store/diglog/repository/ImageRepository.java
@@ -3,7 +3,6 @@ package api.store.diglog.repository;
 import api.store.diglog.model.entity.Image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -11,12 +10,9 @@ import java.util.UUID;
 
 public interface ImageRepository extends JpaRepository<Image, UUID> {
 
-	List<Image> findByUrlIn(List<String> urls);
-
 	List<Image> findByRefId(UUID refId);
 
 	void deleteAllByRefIdAndUrlIn(@Param("refId") UUID refId, @Param("urls") List<String> urls);
 
-	@Modifying
 	List<Image> deleteAllByRefId(UUID refId);
 }
