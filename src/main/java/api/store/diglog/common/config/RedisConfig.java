b47f75578d2b095abfdaaeb88036f9ac24fd25e2
Frod90 <142146516+Frod90@users.noreply.github.com>
2025-05-15 16:44:09 +0900

    fix: 레디스의 자동 빈, 수동 빈 충돌 해결, 레디스 트랜잭션 활성화

--- src/main/java/api/store/diglog/common/config/RedisConfig.java
+++ src/main/java/api/store/diglog/common/config/RedisConfig.java
@@ -6,7 +6,9 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -21,16 +23,21 @@ public class RedisConfig {
 	private int port;
 
 	@Bean
+	@Primary
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+		return new LettuceConnectionFactory(config);
 	}
 
 	@Bean
+	@Primary
 	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(redisConnectionFactory);
-		redisTemplate.setDefaultSerializer(new StringRedisSerializer());
-		return redisTemplate;
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		template.setEnableTransactionSupport(true);
+		template.setDefaultSerializer(new StringRedisSerializer());
+		template.afterPropertiesSet();
+		return template;
 	}
 
 	@Bean
