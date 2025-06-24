bd199a6b92c5b77cf526d4545c4e31682501737d
Frod90 <142146516+Frod90@users.noreply.github.com>
2025-06-24 10:50:25 +0900

    refactor: 스케줄러 설정 분리

--- src/main/java/api/store/diglog/DiglogApplication.java
+++ src/main/java/api/store/diglog/DiglogApplication.java
@@ -2,12 +2,8 @@ package api.store.diglog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync
-@EnableScheduling
 public class DiglogApplication {
 
 	public static void main(String[] args) {
