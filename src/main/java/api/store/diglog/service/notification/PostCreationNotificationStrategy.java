585d35b11370cbcc83d736a71481ae7d230d34fb
Frod90 <142146516+Frod90@users.noreply.github.com>
2025-09-05 08:28:40 +0900

    refactor: 알림 메세지 변경, 게시글 작성자와 구독자가 같은 경우에 대한 필터링 추가

--- src/main/java/api/store/diglog/service/notification/PostCreationNotificationStrategy.java
+++ src/main/java/api/store/diglog/service/notification/PostCreationNotificationStrategy.java
@@ -21,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PostCreationNotificationStrategy implements NotificationStrategy {
 
-	private static final String NOTIFICATION_MESSAGE_FORMAT = "%s님이 \"%s\" 새 게시글을 작성했습니다.";
+	private static final String NOTIFICATION_MESSAGE_FORMAT = "%s님이 \"%s\" 게시글을 작성했습니다.";
 
 	private final PostRepository postRepository;
 	private final SubscriptionRepository subscriptionRepository;
@@ -33,14 +33,15 @@ public class PostCreationNotificationStrategy implements NotificationStrategy {
 
 	@Override
 	public List<Member> resolveReceivers(UUID dataId) {
-		Post post = searchPostBy(dataId);
+		Member author = searchPostBy(dataId).getMember();
 
 		List<Subscription> subscriptions =
-			subscriptionRepository.findAllByAuthorAndSubscriberIsDeletedFalse(post.getMember());
+			subscriptionRepository.findAllByAuthorAndSubscriberIsDeletedFalse(author);
 
 		return subscriptions.stream()
 			.filter(Subscription::isNotificationEnabled)
 			.map(Subscription::getSubscriber)
+			.filter(subscriber -> subscriber.isDifferent(author))
 			.toList();
 	}
 
