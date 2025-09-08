72df38424506b80cf188c9a45d3370e9ddad297e
Frod90 <142146516+Frod90@users.noreply.github.com>
2025-09-08 17:09:41 +0900

    test: 알림 전략 객체 팩토리 테스트 구현

---
+++ src/test/java/api/store/diglog/service/notification/NotificationStrategyFactoryTest.java
@@ -0,0 +1,43 @@
+package api.store.diglog.service.notification;
+
+import static api.store.diglog.model.entity.notification.NotificationType.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import api.store.diglog.model.entity.notification.NotificationType;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class NotificationStrategyFactoryTest {
+
+	@Autowired
+	private NotificationStrategyFactory notificationStrategyFactory;
+
+	@DisplayName("알림 타입에 맞는 전략 객체를 조회할 수 있다.")
+	@ParameterizedTest(name = "{0} 타입은 {1} 전략으로 매핑된다")
+	@MethodSource("provideNotificationTypesAndStrategies")
+	void getStrategy(NotificationType type, Class<? extends NotificationStrategy> expectedClass) {
+		// When
+		NotificationStrategy strategy = notificationStrategyFactory.getStrategy(type);
+
+		// Then
+		assertThat(strategy).isInstanceOf(expectedClass);
+	}
+
+	private static Stream<Arguments> provideNotificationTypesAndStrategies() {
+		return Stream.of(
+			Arguments.of(COMMENT_CREATION, CommentCreationNotificationStrategy.class),
+			Arguments.of(POST_CREATION, PostCreationNotificationStrategy.class),
+			Arguments.of(INVALID, UnsupportedNotificationStrategy.class)
+		);
+	}
+}
